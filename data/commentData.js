440d5f0729c5cd788f06fbb360f74fd9030f3296
LancyLi0211 <lcz.lyp@gmail.com>
2022-12-15 16:14:20 -0500

    data update

---
+++ data/commentData.js
@@ -0,0 +1,95 @@
+const db = require("../config/mongoCollection");
+const userdb = require("./userData");
+const animaldb = require("./animalData");
+const { ObjectId } = require("mongodb");
+
+const creatComment = async (comment, username, animalid) => {
+  let date = new Date();
+  date = date.toUTCString();
+  const commentdb = await db.commentCollection();
+  const User = await userdb.getUserdata(username);
+  const userid = User._id.toString();
+  const commentData = {
+    text: comment,
+    user_id: userid,
+    animal_id: animalid,
+    time: date,
+  };
+  const commentInfo = await commentdb.insertOne(commentData);
+  if (!commentInfo.acknowledged || !commentInfo.insertedId) {
+    throw "Could not add this comment";
+  }
+
+  commentData._id = commentData._id.toString();
+  const insertuser = await userdb.putCommentIn(commentData._id, userid);
+  if (!insertuser) {
+    throw "comment_id insert in user error";
+  }
+  const insertanimal = await animaldb.putCommentIn(commentData._id, animalid);
+  if (!insertanimal) {
+    throw "comment_id insert in animal error";
+  }
+  return commentData;
+};
+
+const getCommentByPostId = async (animalid) => {
+  const animal = await animaldb.getAnimalPostByID(animalid);
+  const commentidList = animal.comment_ids;
+  let commentList = [];
+  for (let index = 0; index < commentidList.length; index++) {
+    const element = commentidList[index];
+    let comment = await getCommentById(element);
+    comment._id = comment._id.toString();
+    commentList.push(comment);
+  }
+  return commentList;
+};
+
+const getCommentByUserId = async (userid) => {
+  const user = await userdb.getUserById(userid);
+  const commentidList = user.comment_ids;
+  let commentList = [];
+  for (let index = 0; index < commentidList.length; index++) {
+    const element = commentidList[index];
+    let comment = await getCommentById(element);
+    comment._id = comment._id.toString();
+    commentList.push(comment);
+  }
+  return commentList;
+};
+
+const getCommentById = async (commentid) => {
+  const commentdb = await db.commentCollection();
+  const comment = await commentdb.findOne({ _id: ObjectId(commentid) });
+  if (!comment) {
+    throw `can not find comment with id of ${commentid}`;
+  }
+  comment._id = comment._id.toString();
+  return comment;
+};
+
+const removeCommentById = async (commentid) => {
+  const commentdb = await db.commentCollection();
+  const commnet = await commentdb.findOne({ _id: ObjectId(commentid) });
+  let userid = commnet.user_id.toString();
+  let animalid = commnet.animal_id.toString();
+  if (!commnet) {
+    throw "This commnet is not exist";
+  }
+  const deletionInfo = await commentdb.deleteOne({ _id: ObjectId(commentid) });
+  if (deletionInfo.deletedCount === 0) {
+    throw `Could not delete comment with id of ${commentid}`;
+  }
+  const removeInfo = await userdb.removeCommentFromU(commentid, userid);
+  const removeInfo2 = await animaldb.removeCommentFromA(commentid, animalid);
+  if (removeInfo && removeInfo2)
+    return `The comment ${commnet._id} has been successfully deleted!`;
+};
+
+module.exports = {
+  creatComment,
+  getCommentByPostId,
+  removeCommentById,
+  getCommentById,
+  getCommentByUserId,
+};
--- data/volunteerData.js
+++ data/volunteerData.js
@@ -1,31 +1,79 @@
-const { volunteerCollection } = require('../config/mongoCollection');
+const db = require("../config/mongoCollection");
+const userdb = require("./userData");
+const { ObjectId } = require("mongodb");
 
+const createVollunteerPost = async (
+  contact,
+  location,
+  type,
+  description,
+  username
+) => {
+  const User = await userdb.getUserdata(username);
+  const userid = User._id.toString();
+  const VollunteerData = {
+    volunteer_contact: contact,
+    volunteer_location: location,
+    volunteer_type: type,
+    volunteer_decription: description,
+    user_id: userid,
+  };
+  const volunteerdb = await db.volunteerCollection();
+  const insertInfo = await volunteerdb.insertOne(VollunteerData);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+    throw "Could not add this comment";
+  }
+  VollunteerData._id = VollunteerData._id.toString();
+  const insertuser = await userdb.putVolunteerIn(VollunteerData._id, userid);
+  if (!insertuser) {
+    throw "comment_id insert error";
+  }
+};
 
+const getAllVollunteerPosts = async (username) => {
+  const user = await userdb.getUserdata(username);
+  let volunteeridList = user.volunteer_ids;
+  let volunteerList = [];
+  for (let index = 0; index < volunteeridList.length; index++) {
+    const element = volunteeridList[index];
+    let volunteer = await getVolunteerById(element);
+    volunteerList.push(volunteer);
+  }
+  return volunteerList;
+};
 
-const createVolunteer = async (name, contact, location, type, decription) => {
+const getVolunteerById = async (volunteerid) => {
+  const volunteerdb = await db.volunteerCollection();
+  const volunteer = await volunteerdb.findOne({ _id: ObjectId(volunteerid) });
+  if (!volunteer) {
+    throw `can not find animal post with id of ${volunteerid}`;
+  }
+  volunteer._id = volunteer._id.toString();
+  return volunteer;
+};
 
-    const volunteerData = {
-        volunteer_name: name,
-        volunteer_contact: contact,
-        volunteer_location: location,
-        volunteer_type: type,
-        volunteer_decription: decription
-    }
+const removeVolunteerById = async (id) => {
+  //volunteer id
+  const volunteerdb = await db.volunteerCollection();
+  const volunteer = await volunteerdb.findOne({ _id: ObjectId(id) });
+  if (!volunteer) {
+    throw `could not find volunteer with id of ${id}`;
+  }
+  let userid = volunteer.user_id.toString();
+  if (!volunteer) {
+    throw "This volunteert post is not exist";
+  }
+  const deletionInfo = await volunteerdb.deleteOne({ _id: ObjectId(id) });
+  if (deletionInfo.deletedCount === 0) {
+    throw `Could not delete volunteert post with id of ${id}`;
+  }
+  const remove = await userdb.removeVolunteerFromU(id, userid);
+  return `The volunteert post ${volunteer._id} has been successfully deleted!`;
+};
 
-    const col = await volunteerCollection();
-
-    const info = await col.insertOne(volunteerData);
-    if (!info.acknowledged || !info.insertedId) throw 'Could not add this volunteer';
-    return { insertedVolunteer: true };
-
-}
-
-const getAllVolunteerPosts = async () => {
-    const col = await volunteerCollection();
-    const postList = await col.find({}).toArray();
-    if (postList.length == 0) throw 'No volunteer in database';
-    return postList;
-}
-
-
-module.exports = { createVolunteer, getAllVolunteerPosts };
+module.exports = {
+  createVollunteerPost,
+  getAllVollunteerPosts,
+  getVolunteerById,
+  removeVolunteerById,
+};
