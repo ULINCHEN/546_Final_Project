638788cfce5e650277ad1d41bcc8e419f46d1142
ULINCHEN <chenyoulinusa@gmail.com>
2022-12-17 17:25:41 -0500

    add put form

--- routes/animalRoutes.js
+++ routes/animalRoutes.js
@@ -8,6 +8,7 @@ const commentData = data.commentData;
 const router = express.Router();
 const maxsize = 16777216;
 const multer = require("multer");
+const { JSONParser } = require('formidable/lib/json_parser');
 const upload = multer({
     dest: "public/uploads/",
     limits: { fileSize: maxsize },
@@ -21,8 +22,8 @@ router.route("/")
     .get(async (req, res) => {
         //code here for GET  
         let login = false;
-        if (req.session.user) 
-            login = true;    
+        if (req.session.user)
+            login = true;
         try {
             const postData = await animalData.getAllAnimalPosts();
             //console.log(postData);
@@ -41,8 +42,8 @@ router.route("/")
     .post(async (req, res) => { //search by type
         let type = xss(req.body.type);
         let login = false;
-        if (req.session.user) 
-            login = true; 
+        if (req.session.user)
+            login = true;
         try {
             const postData = await animalData.getAnimalByType(type);
             res.render('animalPosts', {
@@ -62,8 +63,8 @@ router.route("/")
 router.route("/location/:location")
     .get(async (req, res) => {
         let login = false;
-        if (req.session.user) 
-            login = true; 
+        if (req.session.user)
+            login = true;
         const location = req.params.location;
         const postData = await animalData.getAllAnimalPosts();
         // 这里要做一个判断 如果数据库有这个地址，则可以访问，否则导向错误页面
@@ -80,8 +81,8 @@ router.route("/detail/:id")
         //code here for GET
         let id = req.params.id;
         let login = false;
-        if (req.session.user) 
-            login = true; 
+        if (req.session.user)
+            login = true;
         try {
             let post = await animalData.getAnimalPostById(id);
             let comments = await commentData.getCommentByPostId(id);
@@ -100,7 +101,7 @@ router.route("/detail/:id")
         }
     })
     .post(async (req, res) => {
-        if (req.session.user){
+        if (req.session.user) {
             let animal_id = req.params.id;
             let text = xss(req.body.newComment);
             let username = req.session.user.username;
@@ -109,7 +110,7 @@ router.route("/detail/:id")
             console.log(req.body);
             try {
                 const comment = await commentData.createComment(text, username, animal_id);
-                
+
             } catch (e) {
                 res.status(400);
                 return res.render('error', {
@@ -135,14 +136,14 @@ router.route("/detail/:id")
             }
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to comment.',
                 login: false
-            }); 
+            });
         }
     })
     .delete(async (req, res) => {
-        if (req.session.user){
+        if (req.session.user) {
             //code here for DELETE
             let post_id = null;
             try {
@@ -151,10 +152,10 @@ router.route("/detail/:id")
                 const user_id = postData.user_id;
                 if (req.session.user.userid !== user_id) throw 'Please login to delete your animal post.';
             } catch (e) {
-                return res.render('error', { 
+                return res.render('error', {
                     errorMsg: e,
                     login: true
-                }); 
+                });
             }
             try {
                 await commentData.removeCommentByA(post_id);
@@ -162,58 +163,60 @@ router.route("/detail/:id")
                 res.status(200)
                 return res.redirect('/user/userCenter/' + post_id);
             } catch (e) {
-                return res.render('error', { 
+                return res.render('error', {
                     errorMsg: e,
                     login: true
-                }); 
+                });
             }
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to delete your animal post.',
                 login: true
-            }); 
+            });
         }
     });
 
 router.route("/edit/:id")
     .get(async (req, res) => {
-        if (req.session.user){
-            try{
+        if (req.session.user) {
+            try {
                 const post_id = req.params.id;
                 const postData = await animalData.getAnimalPostById(post_id);
                 const user_id = postData.user_id;
+                const putUrl = "animal/edit/" + post_id + "?_method=PUT";
                 if (req.session.user.userid !== user_id) throw 'Please login to edit your animal post.';
                 return res.render('editAnimalPost', {
                     title: "Edit your animal post",
                     postData: postData,
-                    url: "/animal/edit/" + post_id + "?_method=PUT",
+                    url: putUrl,
                     login: true
                 });
-            } catch(e) {
-                return res.render('error', { 
+            } catch (e) {
+                return res.render('error', {
                     errorMsg: e,
                     login: true
                 });
-            }  
+            }
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to edit your animal post.',
                 login: false
-            }); 
+            });
         }
     })
-    .put(async (req, res) => { 
-        if (req.session.user){
-            let animalName =null;
+    .put(async (req, res) => {
+        console.log("Put body: ", req.body);
+        if (req.session.user) {
+            let animalName = null;
             let species = null;
             let description = null;
             let healthCondition = null;
             let location = null;
             //console.log(location);
 
-            try{
+            try {
                 let post_id = req.params.id;
                 const animalPost = animalData.getAnimalPostById(post_id);
                 const user_id = animalPost.user_id;
@@ -225,14 +228,14 @@ router.route("/edit/:id")
                 location = xss(req.body.location);
                 // animal photo can be empty
                 // location check
-            } catch(e) {
+            } catch (e) {
                 res.status(400);
-                return res.render('editAnimalPost',  {
+                return res.render('editAnimalPost', {
                     error: e,
                     login: true
                 });
             }
-            
+
             try {
                 const new_animal_post = await animalData.updateAnimalPost(
                     post_id,
@@ -247,20 +250,20 @@ router.route("/edit/:id")
                 return res.redirect('/animal/edit/' + id);
             } catch (e) {
                 res.status(500);
-                return res.render('editAnimalPos',  {
+                return res.render('editAnimalPos', {
                     error: e,
                     login: true
                 });
             }
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to edit your post.',
                 login: false
-            }); 
+            });
         }
     });
-    
+
 router.route("/new")
     .get(async (req, res) => {
         //code here for GET
@@ -271,14 +274,14 @@ router.route("/new")
             });
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to add a new animal post.',
                 login: false
-            }); 
+            });
         }
     })
-    .post(upload.single("photo1"),async (req, res) => {
-        if (req.session.user){
+    .post(upload.single("photo1"), async (req, res) => {
+        if (req.session.user) {
             let animalName = xss(req.body.animal_name);
             let species = xss(req.body.species);
             let description = xss(req.body.description);
@@ -287,23 +290,23 @@ router.route("/new")
             let userid = req.session.user.userid;
             console.log(req.body);
             //[xss(req.body.photo1), xss(req.body.photo2), xss(req.body.photo3)];
-                        
+
             //console.log(location);
 
-            try{
+            try {
                 animalName = publicMethods.checkName(animalName, "Animal Name");
                 [species, healthCondition] = publicMethods.checkAnimalPost(species, healthCondition);
                 description = publicMethods.checkArticle(description);
                 // animal photo can be empty
                 // location check
-            } catch(e) {
+            } catch (e) {
                 res.status(400);
-                return res.render('addPost',  {
+                return res.render('addPost', {
                     error: e,
                     login: true
                 });
             }
-            
+
             try {
                 const new_animal_post = await animalData.createAnimalPost(
                     animalName,
@@ -318,28 +321,28 @@ router.route("/new")
                 return res.redirect('/animal/detail/' + new_animal_post.animalid);
             } catch (e) {
                 res.status(500);
-                return res.render('addPost',  {
+                return res.render('addPost', {
                     error: e,
                     login: true
                 });
             }
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to add new post.',
                 login: false
-            }); 
+            });
         }
     });
 
 router.route("/follow/:id")
     .post(async (req, res) => {
-        if (req.session.user){
+        if (req.session.user) {
             let post_id = req.params.id;
             let postData = animalData.getAnimalPostById(post_id);
             try {
                 if (req.session.user.userid === postData.user_id) throw "You can not follow the animal you have posted.";
-                const follow = await animalData.putFollowInUser(post_id, req.session.user.userid) 
+                const follow = await animalData.putFollowInUser(post_id, req.session.user.userid)
             } catch (e) {
                 res.status(400);
                 return res.render('error', {
@@ -366,10 +369,10 @@ router.route("/follow/:id")
             }
         } else {
             res.status(400);
-            return res.render('error', { 
+            return res.render('error', {
                 errorMsg: 'Please login to follow.',
                 login: false
-            }); 
+            });
         }
     });
 module.exports = router;
