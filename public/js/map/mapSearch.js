6e4c55b2ddd3ef62dffeaa508cd541d99d3c4704
Wei Guo <qiangzheguo@gmail.com>
2022-12-18 17:31:35 -0500

    change mapSearch locaiton in file
    change mapSearch locaiton in file

--- public/js/map/mapSearch.js
+++ public/js/map/mapSearch.js
@@ -16,20 +16,19 @@ $(function () {
         i, a, title, marker;
 
     mcgLayerSupportGroup.addTo(map);
-
     infoArray.forEach(function (info) {
         var id = info.id;
         var name = info.name;
         var date = info.date;
-        var healfh = info.health;
+        var healfh = info.health.toLowerCase();
         var lat = info.lat;
         var lng = info.lng;
         var description = info.description;
         var url = info.url;
-        var species = info.species;
+        var species = info.species.toLowerCase();
         if (healfh == "bad") {
             var redIcon = new L.Icon({
-            iconUrl: '/public/images/marker-icon-2x-red.png',
+            iconUrl: '../../public/images/marker-icon-2x-red.png',
             shadowUrl: '/public/images/marker-shadow.png',
             iconSize: [25, 41],
             iconAnchor: [12, 41],
